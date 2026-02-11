9a63ab2d6e9c1e20e537c297a7aa3815f3678317
Ethan Wilde <ewilde@santarosa.edu>
2026-02-11 09:25:03 -0800

    Initial commit.

---
+++ script-complete.js
@@ -0,0 +1,129 @@
+"use strict";
+
+// simplified steps for making a Phaser-based game with one scene
+
+// 1. defining a set of game configuration properties
+const config = {
+  type: Phaser.WEBGL,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade'
+  },
+  scene: {
+    preload: myPreload,
+    create: myCreate,
+    update: myUpdate
+  }
+};
+
+// 2. define global variables 
+  // simple numbers to track game economy metrics like score, how balls the player has
+let score, balls_available;
+  // reference to game objects we create that we need to do stuff with from time to time
+let ball, paddle, bricks;
+
+// 3. We have at least three functions to successfully create a single scene in our game: preload
+function myPreload() {
+  // load 3 assets
+  this.load.image('ball_image','img/ball.png');
+  this.load.image('brick_image','img/brick.png');
+  this.load.image('paddle_image','img/paddle.png');
+}
+
+// 4. create
+function myCreate() {
+  // make the edges of stage collidable so ball bounces off them
+  this.physics.world.setBoundsCollision( true, true, true, false );
+
+  // create the first game object: ball
+  ball = this.physics.add.sprite( 400, 500, 'ball_image' );
+  ball.setCollideWorldBounds( true );
+  ball.setBounce(1); // 0-1 where 1=100% of the inertial energy goes back into the ball
+
+  // make the paddle
+  paddle = this.physics.add.sprite( 400, 550, 'paddle_image' );
+  paddle.setScale( 2, 1 );
+  paddle.setImmovable();
+
+  // make a group of bricks
+  bricks = this.physics.add.group(
+    {
+      key: 'brick_image',
+      quantity: 120,
+      gridAlign: {
+        width: 20,
+        height: 6,
+        cellWidth: 32,
+        cellHeight: 48,
+        x: 112,
+        y: 100
+      }
+    }
+  );
+
+  bricks.children.iterate(
+    function(one_brick) {
+      one_brick.setImmovable();
+    }
+  );
+
+  // now establish the collisions we care about detecting: 
+
+    // ball touches paddle
+    this.physics.add.collider( ball, paddle, hitPaddle, null, this );
+
+    // ball touches a brick in the bricks group
+    this.physics.add.collider( ball, bricks, hitBrick, null, this );
+
+  // manage user input using on() event handler
+  this.input.on( 'pointermove', movePaddle, this );
+  this.input.on( 'pointerup', launchBall, this );
+
+  // initialze some numeric values for score, and balls
+  score = 0;
+  balls_available = 3;
+}
+
+// 5. update (run every frame of game loop)
+function myUpdate() {
+  if ( ball.y > 600 ) {
+    console.log('ball lost!');
+    balls_available--;
+    if (balls_available > 0) {
+      ball.x = 400;
+      ball.y = 500;
+      ball.setVelocity(0,0);
+    } else {
+      console.log('game over!');
+      ball.y = 600;
+      ball.setVelocity(0,0);
+    }
+  }
+}
+
+// 6. Create a new phaser game object
+const mygame = new Phaser.Game( config );
+
+// 7. We might also define other custom functions
+
+// custom event handler called by phaser during game loop when ball + paddle touch
+function hitPaddle() {
+  console.log('ball hit paddle');
+}
+
+function movePaddle( pointer ) {
+  paddle.x = pointer.x;
+}
+
+function hitBrick( ball_hit, brick_hit ) {
+  brick_hit.disableBody(true, true);
+  score = score + 10;
+  console.log(score);
+}
+
+function launchBall() {
+  if (balls_available > 0) {
+    ball.setVelocity( -75, -300 );
+  }
+}
---
+++ week5-demo-w-2-balls/script-animated.js
@@ -0,0 +1,185 @@
+"use strict";
+
+// simplified steps for making a Phaser-based game with one scene
+
+// 1. defining a set of game configuration properties
+const config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 600,
+    physics: {
+      default: 'arcade'
+    },
+    scene: {
+      preload: myPreload,
+      create: myCreate,
+      update: myUpdate
+    }
+  };
+
+// 2. define global variables
+  // simple numbers to track game economy metrics like score, how balls the player has
+let score_value, balls_available;
+  // reference to game objects we create that we need to do stuff with from time to time
+let ball, paddle, bricks;
+  // create a second ball: global variable
+let ball2;
+
+// 3. We have at least three functions to successfully create a single scene in our game: preload
+function myPreload() {
+    // load 3 assets
+    this.load.image('ball_image','img/ball.png');
+    this.load.image('brick_image','img/brick.png');
+    this.load.image('paddle_image','img/paddle.png');
+    // create a second ball: load a 2-frame 32x32px spritesheet for 2nd ball
+    this.load.spritesheet(
+      'ball2_spritesheet',
+      'img/orange_fireball.png',
+      {
+        frameWidth: 32,
+        frameHeight: 32
+      }
+    );
+}
+
+// 4. create
+function myCreate() {
+    // make the edges of stage collidable so ball bounces off them
+    this.physics.world.setBoundsCollision( true, true, true, false );
+
+    // create the first game object: ball
+    ball = this.physics.add.sprite( 400, 500, 'ball_image' );
+    ball.setCollideWorldBounds( true );
+    ball.setBounce(1); // 0-1 where 1=100% of the inertial energy goes back into the ball
+
+    // create a second ball: make game object off screen
+    ball2 = this.physics.add.sprite( 900, 500, 'ball2_spritesheet' );
+    ball2.setCollideWorldBounds( false );
+    ball2.setBounce(1);
+    let ball_anim_frames = this.anims.generateFrameNumbers(
+        'ball2_spritesheet',
+        {
+        start: 0,
+        end: 1
+        }
+    );
+    let ball_animation = this.anims.create(
+        {
+        key: 'flaming',
+        frames: ball_anim_frames,
+        frameRate: 15,
+        repeat: -1,
+        yoyo: false
+        }
+    );
+    ball2.anims.play('flaming');
+    this.tweens.add(
+        {
+            targets: ball2,
+            duration: 1000,
+            repeat: -1,
+            yoyo: true,
+            angle: '-=360'
+        }
+    );
+
+    // make the paddle
+    paddle = this.physics.add.sprite( 400, 550, 'paddle_image' );
+    paddle.setScale( 2, 1 );
+    paddle.setImmovable();
+
+    // make a group of bricks
+    bricks = this.physics.add.group(
+        {
+            key: 'brick_image',
+            quantity: 120,
+            gridAlign: {
+                width: 20,
+                height: 6,
+                cellWidth: 32,
+                cellHeight: 48,
+                x: 112,
+                y: 100
+            }
+        }
+    );
+
+    bricks.children.iterate(
+        function(one_brick) {
+            one_brick.setImmovable();
+        }
+    );
+
+    // now establish the collisions we care about detecting: 
+
+        // ball touches paddle
+        this.physics.add.collider( ball, paddle, hitPaddle, null, this );
+
+        // ball touches a brick in the bricks group
+        this.physics.add.collider( ball, bricks, hitBrick, null, this );
+
+        // create a second ball: add collision detection
+        this.physics.add.collider( ball2, paddle, hitPaddle, null, this );
+        this.physics.add.collider( ball2, bricks, hitBrick, null, this );
+
+    // manage user input using on() event handler (pointer)
+    this.input.on( 'pointermove', movePaddle, this );
+    this.input.on( 'pointerup', launchBall, this );
+
+    // initialze some numeric values for score, and balls
+    score_value = 0;
+    balls_available = 3;
+}
+
+// 5. update (run every frame of game loop)
+function myUpdate() {
+    if ( ball.y > 600 ) {
+        console.log('ball lost!');
+        balls_available--;
+        if (balls_available > 0) {
+            ball.x = 400;
+            ball.y = 500;
+            ball.setVelocity(0,0);
+        } else {
+            console.log('game over!');
+            ball.y = 600;
+            ball.setVelocity(0,0);
+        }
+    }
+    // create a second ball: check if ball went past paddle
+    if ( ball2.y > 600 ) {
+        console.log('ball2 lost!');
+        ball2.setVelocity(0,0);
+        ball2.setCollideWorldBounds( false );
+        ball2.x = 900;
+        ball2.y = 500;
+    }
+}
+
+// 6. Create a new phaser game object
+const mygame = new Phaser.Game( config );
+
+// 7. We might also define other custom functions
+function hitPaddle() {
+    console.log('ball hit paddle');
+}
+function hitBrick( ball_hit, brick_hit ) {
+    brick_hit.disableBody(true, true);
+    score_value = score_value + 10;
+    // create a second ball: put ball in play if score is 100
+    if (score_value === 100) {
+        ball2.x = 400;
+        ball2.y = 500;
+        ball2.setVelocity( 75, -300 );
+        ball2.setCollideWorldBounds( true );
+    }
+    console.log(score_value);
+}
+function movePaddle( pointer ) {
+    paddle.x = pointer.x;
+}
+function launchBall() {
+    if (balls_available > 0) {
+        ball.setVelocity( -75, -300 );
+    }
+}
